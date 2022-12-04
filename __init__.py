526784f1bceb9455442eea82fea68a83fbfe3fdb
Emir <emiralic22@gmail.com>
2022-12-04 10:01:11 +0100

    Create your profile with email, name and password and it will be legit with login

--- __init__.py
+++ __init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 
-# init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
 
 def create_app():
@@ -23,11 +22,9 @@ def create_app():
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-    # blueprint for auth routes in our app
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
-    # blueprint for non-auth parts of app
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
